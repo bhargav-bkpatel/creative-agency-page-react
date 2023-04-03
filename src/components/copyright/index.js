20ce41700bd305b704ab0fea7ed75a68a8b75a7f
Bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2023-04-03 17:26:47 +0530

    Update

--- src/components/copyright/index.js
+++ src/components/copyright/index.js
@@ -12,76 +12,17 @@ const Copyright = (props) => {
           <span className="copyright-text01">
             <span>
               © All rights reserved
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
+             
             </span>
-            <span>
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
+            
           </span>
-          <a
-            href="https://www.teleporthq.io"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text04">@TeleportHQ.</span>
-          </a>
-          <span className="copyright-text05">
-            <span>
-              {' '}
-              Powered by
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
-            <span>
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
-          </span>
-          <a
-            href="https://www.vercel.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text08">Vercel</span>
-          </a>
-          <span className="copyright-text09">
-            <span>
-              . Image source:
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
-            <span>
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: ' ',
-                }}
-              />
-            </span>
-          </span>
-          <a
-            href="https://www.unsplash.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <span className="copyright-text12">Unsplash</span>
-          </a>
+          
+            <span className="copyright-text04">@Bhargav.</span>
+         
+        
+          
+          
+  
           <span className="copyright-text13">.</span>
         </span>
       </div>
